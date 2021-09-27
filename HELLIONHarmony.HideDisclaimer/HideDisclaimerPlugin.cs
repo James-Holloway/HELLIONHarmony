2af20ce36c1f391d30c0c4a1f91560fb24827a0e
James-Holloway <james.holloway@pyrites.org.uk>
2021-09-27 04:46:04 +0100

    Created HELLIONHarmony.HideDisclaimer plugin Made as another example and a utility to speed up game debugging

---
+++ HELLIONHarmony.HideDisclaimer/HideDisclaimerPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HELLIONHarmony;
+
+namespace HELLIONHarmony.HideDisclaimer
+{
+    public class HideDisclaimerPlugin : Plugin, IMenuPlugin
+    {
+        public override string Identifier => "HELLIONHarmony.HideDisclaimer";
+
+        public override string Author => "James Holloway";
+
+        public override string Description => "A simple plugin to never show the disclaimer on startup";
+
+        public override PatchScope OnlyScope => PatchScope.Client;
+    }
+}
---
+++ HELLIONHarmony.HideDisclaimer/SkipDisclaimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HELLIONHarmony;
+
+namespace HELLIONHarmony.HideDisclaimer
+{
+    [HELLIONPatch(PatchScope.Client, typeof(ZeroGravity.CanvasManager), "Start")]
+    public class SkipDisclaimer
+    {
+        public static void Prefix()
+        {
+            ZeroGravity.CanvasManager.ShowDisclamer = false;
+        }
+    }
+}
